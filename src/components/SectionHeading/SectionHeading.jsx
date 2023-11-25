c9c4629dbe04112f65b093c1571ed94adf6ced1a
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-25 15:38:38 +0600

    section title added

--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 
-const SectionHeading = ({heading}) => {
+const SectionHeading = ({ heading }) => {
     return (
         <div className="mx-auto text-center md:w-4/12 my-8">
-            <p className="text-cyan-700-600 mb-2">--- MEDICO ---</p>
+            <p className="text-cyan-700 mb-2">--- MEDICO ---</p>
             <h3 className="text-3xl text-cyan-950 font-semibold uppercase border-y-4 py-4">{heading}</h3>
         </div>
     );
